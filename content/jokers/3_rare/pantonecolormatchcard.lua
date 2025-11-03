cddec8571181cda1c884169b2d4342b1ddccb972
MetaNite64 <noahtrivedi@gmail.com>
2025-11-03 01:30:10 -0600

    code Pantone Color Match Card

--- content/jokers/3_rare/pantonecolormatchcard.lua
+++ content/jokers/3_rare/pantonecolormatchcard.lua
@@ -33,8 +33,52 @@ SMODS.Joker{ --Pantone Color Match Card
     },
 
     calculate = function(self, card, context)
-        if context.cardarea == G.jokers and context.joker_main  then
+        if context.cardarea == G.jokers and context.joker_main then
             if next(context.poker_hands["Flush"]) then
+                local suit = pseudorandom_element(SMODS.Suits, pseudoseed("pantone_suit"))
+                for i = 1, #G.hand.cards do
+                    local percent = 1.15 - (i - 0.999) / (#G.hand.cards - 0.998) * 0.3
+                    local v = G.hand.cards[i]
+                    G.E_MANAGER:add_event(Event({
+                        trigger = "after",
+                        delay = 0.15,
+                        func = function()
+                            v:flip()
+                            play_sound("card1", percent)
+                            v:juice_up(0.3, 0.3)
+                            return true
+                        end
+                    }))
+                end
+                delay(0.2)
+                for i, v in pairs(G.hand.cards) do
+                    G.E_MANAGER:add_event(Event({
+                        trigger = "after",
+                        delay = 0.1,
+                        func = function()
+                            assert(SMODS.change_base(v, suit.key))
+                            return true
+                        end
+                    }))
+                end
+                for i = 1, #G.hand.cards do
+                    local percent = 0.85 + (i - 0.999) / (#G.hand.cards - 0.998) * 0.3
+                    local v = G.hand.cards[i]
+                    G.E_MANAGER:add_event(Event({
+                        trigger = "after",
+                        delay = 0.15,
+                        func = function()
+                            v:flip()
+                            play_sound("tarot2", percent, 0.6)
+                            v:juice_up(0.3, 0.3)
+                            return true
+                        end
+                    }))
+                end
+                return {
+                    message = localize(suit.key, "suits_plural") .. "!",
+                    colour = G.C.SUITS[suit.key]
+                }
             end
         end
     end
